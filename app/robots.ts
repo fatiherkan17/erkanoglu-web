f0f6b8e17ef80035e6f267b8b952587cbc75b144
fatiherkan17 <fatih_erkan_17@hotmail.com>
2026-09-01 21:28:33 +0300

    Add robots policy and sitemap reference

---
+++ app/robots.ts
@@ -0,0 +1,8 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [{ userAgent: "*", allow: "/" }],
+    sitemap: "https://erkanoglu.com.tr/sitemap.xml",
+  };
+}
